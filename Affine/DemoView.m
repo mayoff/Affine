627175eee98496ac2ed564309d0824aee6d3e0f8
Rob Mayoff <git@rob.dqd.com>
2012-07-29 02:18:44 -0500

    draw grid and vectors

--- Affine/DemoView.m
+++ Affine/DemoView.m
@@ -8,6 +8,8 @@ Copyright (c) 2012 Rob Mayoff. All rights reserved.
 #import "Model.h"
 #import "UIView+translate.h"
 
+static const CGFloat kScale = 128.0f;
+
 @interface DemoView () <ModelObserver>
 @end
 
@@ -44,8 +46,10 @@ Copyright (c) 2012 Rob Mayoff. All rights reserved.
 }
 
 - (void)drawRect:(CGRect)rect {
-    [self updateCTM];
+    CGContextConcatCTM(UIGraphicsGetCurrentContext(), _uiTransform);
     [self fillWithImage];
+    [self drawUntransformedGrid];
+    [self drawVectors];
 }
 
 #pragma mark - ModelObserver protocol
@@ -55,7 +59,113 @@ Copyright (c) 2012 Rob Mayoff. All rights reserved.
     [self setNeedsDisplay];
 }
 
-#pragma mark - Implementation details
+#pragma mark - Layout details
+
+- (void)layoutLocators {
+    CGAffineTransform t = model_.interpolatedTransform;
+    originLocator_.center = CGPointApplyAffineTransform(CGPointMake(t.tx, t.ty), _uiTransform);
+    uLocator_.center = CGPointApplyAffineTransform(CGPointMake(t.a + t.tx, t.b + t.ty), _uiTransform);
+    vLocator_.center = CGPointApplyAffineTransform(CGPointMake(t.c + t.tx, t.d + t.ty), _uiTransform);
+}
+
+- (void)updateUITransform {
+    CGRect bounds = self.bounds;
+    _uiTransform = CGAffineTransformIdentity;
+    _uiTransform = CGAffineTransformTranslate(_uiTransform, CGRectGetMidX(bounds), CGRectGetMidY(bounds));
+    _uiTransform = CGAffineTransformScale(_uiTransform, kScale, -kScale);
+}
+
+#pragma mark - Drawing details
+
+- (void)updateCTM {
+    CGContextRef gc = UIGraphicsGetCurrentContext();
+    CGContextConcatCTM(gc, model_.interpolatedTransform);
+}
+
+- (void)fillWithImage {
+    if (_imageFill == ImageFillOption_None)
+        return;
+    
+    CGContextRef gc = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(gc); {
+        CGContextConcatCTM(gc, model_.interpolatedTransform);
+        switch (_imageFill) {
+            case ImageFillOption_One:
+                CGContextDrawImage(gc, CGRectMake(0, 0, 1, 1), [self image].CGImage);
+                break;
+            case imageFillOption_Many:
+                CGContextDrawTiledImage(gc, CGRectMake(0, 0, 1, 1), [self image].CGImage);
+                break;
+            default:
+                break;
+        }
+    } CGContextRestoreGState(gc);
+}
+
+- (UIImage *)image {
+    static UIImage *theImage;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        theImage = [UIImage imageNamed:@"Picture.jpg"];
+    });
+    return theImage;
+}
+
+- (void)drawUntransformedGrid {
+    CGContextRef gc = UIGraphicsGetCurrentContext();
+    CGRect bounds = CGRectIntegral(CGContextGetClipBoundingBox(gc));
+    [[UIColor blackColor] setFill];
+    for (CGFloat y = CGRectGetMinY(bounds), yMax = CGRectGetMaxY(bounds); y < yMax; y += 1.0f) {
+        UIRectFill(CGRectMake(bounds.origin.x, y, bounds.size.width, 1.0f / kScale));
+    }
+    for (CGFloat x = CGRectGetMinX(bounds), xMax = CGRectGetMaxX(bounds); x < xMax; x += 1.0f) {
+        UIRectFill(CGRectMake(x, bounds.origin.y, 1.0f / kScale, bounds.size.height));
+    }
+}
+
+- (void)drawVectors {
+    CGContextRef gc = UIGraphicsGetCurrentContext();
+    CGAffineTransform t = model_.interpolatedTransform;
+    [[UIColor blackColor] set];
+    CGContextSetLineCap(gc, kCGLineCapSquare);
+    CGContextSetLineJoin(gc, kCGLineJoinMiter);
+    CGPoint origin = CGPointMake(t.tx, t.ty);
+    [self drawVector:origin startingAtPoint:CGPointZero];
+    [self drawVector:CGPointMake(t.a, t.b) startingAtPoint:origin];
+    [self drawVector:CGPointMake(t.c, t.d) startingAtPoint:origin];
+}
+
+- (void)drawVector:(CGPoint)vector startingAtPoint:(CGPoint)start {
+    CGFloat scale = hypotf(vector.x, vector.y);
+    static const CGFloat kArrowHeadLength = 0.2f;
+    if (scale < kArrowHeadLength)
+        return;
+    CGFloat x = 1.0f - kArrowHeadLength / scale;
+    CGFloat y = 0.05f / scale;
+
+    CGContextRef gc = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(gc); {
+        CGContextTranslateCTM(gc, start.x, start.y);
+        CGContextRotateCTM(gc, atan2f(vector.y, vector.x));
+        CGContextScaleCTM(gc, scale, scale);
+
+        CGContextSetLineWidth(gc, 3.0 / kScale / scale);
+        CGContextBeginPath(gc);
+        CGContextMoveToPoint(gc, 0, 0);
+        CGContextAddLineToPoint(gc, x, 0);
+        CGContextStrokePath(gc);
+
+        CGContextBeginPath(gc);
+        CGContextMoveToPoint(gc, 1, 0);
+        CGContextAddLineToPoint(gc, x, y);
+        CGContextAddLineToPoint(gc, x, -y);
+        CGContextClosePath(gc);
+        CGContextFillPath(gc);
+
+    } CGContextRestoreGState(gc);
+}
+
+#pragma mark - Locator dragging details
 
 - (CGAffineTransform)transformFromLocators {
     CGAffineTransform inverseUITransform = CGAffineTransformInvert(_uiTransform);
@@ -73,49 +183,6 @@ Copyright (c) 2012 Rob Mayoff. All rights reserved.
     t.d = v.y - origin.y;
 
     return t;
-}
-
-- (void)layoutLocators {
-    CGAffineTransform t = model_.interpolatedTransform;
-    originLocator_.center = CGPointApplyAffineTransform(CGPointMake(t.tx, t.ty), _uiTransform);
-    uLocator_.center = CGPointApplyAffineTransform(CGPointMake(t.a + t.tx, t.b + t.ty), _uiTransform);
-    vLocator_.center = CGPointApplyAffineTransform(CGPointMake(t.c + t.tx, t.d + t.ty), _uiTransform);
-}
-
-- (void)updateUITransform {
-    static const CGFloat kScale = 128.0f;
-    CGRect bounds = self.bounds;
-    _uiTransform = CGAffineTransformIdentity;
-    _uiTransform = CGAffineTransformTranslate(_uiTransform, CGRectGetMidX(bounds), CGRectGetMidY(bounds));
-    _uiTransform = CGAffineTransformScale(_uiTransform, kScale, -kScale);
-}
-
-- (void)updateCTM {
-    CGContextRef gc = UIGraphicsGetCurrentContext();
-    CGContextConcatCTM(gc, _uiTransform);
-    CGContextConcatCTM(gc, model_.interpolatedTransform);
-}
-
-- (void)fillWithImage {
-    switch (self.imageFill) {
-        case ImageFillOption_One:
-            CGContextDrawImage(UIGraphicsGetCurrentContext(), CGRectMake(0, 0, 1, 1), [self image].CGImage);
-            break;
-        case imageFillOption_Many:
-            CGContextDrawTiledImage(UIGraphicsGetCurrentContext(), CGRectMake(0, 0, 1, 1), [self image].CGImage);
-            break;
-        default:
-            break;
-    }
-}
-
-- (UIImage *)image {
-    static UIImage *theImage;
-    static dispatch_once_t once;
-    dispatch_once(&once, ^{
-        theImage = [UIImage imageNamed:@"Picture.jpg"];
-    });
-    return theImage;
 }
 
 - (IBAction)pannerDidUpdate:(UIPanGestureRecognizer *)panner {
